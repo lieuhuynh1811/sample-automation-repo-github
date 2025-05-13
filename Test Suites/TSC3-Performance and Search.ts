65a168b976dda7e1d0e58582e3359b2268311525
lieu.huynh@katalon.com <katalon_g3_dummy_username>
2025-05-13 15:39:10 +0000

    Delete "Error Handling and Alerts Suite" test suite

--- Test Suites/TSC3-Performance and Search.ts
+++ Test Suites/TSC3-Performance and Search.ts
@@ -15,16 +15,6 @@
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
-         <testSuiteEntity>Test Suites/Error Handling and Alerts Suite</testSuiteEntity>
-      </TestSuiteRunConfiguration>
-      <TestSuiteRunConfiguration>
-         <configuration>
-            <groupName>Web Desktop</groupName>
-            <profileName>default</profileName>
-            <requireConfigurationData>false</requireConfigurationData>
-            <runConfigurationId>Chrome</runConfigurationId>
-         </configuration>
-         <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/Form Validation Scenarios</testSuiteEntity>
       </TestSuiteRunConfiguration>
    </testSuiteRunConfigurations>
