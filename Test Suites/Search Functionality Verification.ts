0deb77e4d6a1ed84976b149ee35e0681a4be8260
qeautomationkatalon@gmail.com <katalon_g3_dummy_username>
2025-05-30 06:11:24 +0000

    Rename Test Case 300525130953

--- Test Suites/Search Functionality Verification.ts
+++ Test Suites/Search Functionality Verification.ts
@@ -67,7 +67,7 @@
       <guid>af2f2c0c-068b-43af-83b0-ec7cf78faa6b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Verify the search functionality returns relevant results for a given keyword</testCaseId>
+      <testCaseId>Test Cases/_Verify the search functionality returns relevant results for a given keyword(),.- updated</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
