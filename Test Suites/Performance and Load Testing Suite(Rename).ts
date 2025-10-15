8a361b433c0832ab1cd17ce9379a349afc536cab
lieu.huynh@katalon.com <katalon_g3_dummy_username>
2025-10-15 11:01:02 +0000

    Rename test suite

--- Test Suites/Performance and Load Testing Suite(Rename).ts
+++ Test Suites/Performance and Load Testing Suite(Rename).ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Performance and Load Testing Suite</name>
+   <name>Performance and Load Testing Suite(Rename)</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
