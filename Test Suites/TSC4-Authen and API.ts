0a92597a6bd1b0d57448925d638e188f9d28c2e3
lieu.huynh@katalon.com <katalon_g3_dummy_username>
2025-10-15 10:03:25 +0000

    Delete "Error Handling and Alerts Suite(Only Edit TS Name)" test suite

--- Test Suites/TSC4-Authen and API.ts
+++ Test Suites/TSC4-Authen and API.ts
@@ -15,16 +15,6 @@
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
-         <testSuiteEntity>Test Suites/Error Handling and Alerts Suite(Only Edit TS Name)</testSuiteEntity>
-      </TestSuiteRunConfiguration>
-      <TestSuiteRunConfiguration>
-         <configuration>
-            <groupName>Web Desktop</groupName>
-            <profileName>default</profileName>
-            <requireConfigurationData>false</requireConfigurationData>
-            <runConfigurationId>Chrome</runConfigurationId>
-         </configuration>
-         <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/Form Validation Scenarios(Update TS name, Add TC, Remove TC)</testSuiteEntity>
       </TestSuiteRunConfiguration>
    </testSuiteRunConfigurations>
