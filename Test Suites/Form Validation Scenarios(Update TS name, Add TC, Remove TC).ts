8fb283a3402eccb3a22dadffd98dc469aaa29b55
lieu.huynh@katalon.com <katalon_g3_dummy_username>
2025-10-15 10:41:40 +0000

    Edit "Form Validation Scenarios(Update TS name, Add TC, Remove TC)" test suite

--- Test Suites/Form Validation Scenarios(Update TS name, Add TC, Remove TC).ts
+++ Test Suites/Form Validation Scenarios(Update TS name, Add TC, Remove TC).ts
@@ -12,25 +12,6 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>f35d05af-5354-4c30-9e14-026fe4cf9782</testSuiteGuid>
    <testCaseLink>
-      <guid>d249c115-903a-4985-968d-62a8c7160741</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Main Test Cases/Verify that data entered in a form is correctly saved and displayed after submission</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>269060e5-49c4-4771-af87-4bebef169502</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>bf303a06-fe36-4a87-b581-5dbfb9794c78</variableId>
-      </variableLink>
-   </testCaseLink>
-   <testCaseLink>
       <guid>277908ea-68b0-445e-85cd-d5df3e832386</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
