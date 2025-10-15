ccf9dd80d49490990fe55dfc099126f621b56c6d
lieu.huynh@katalon.com <katalon_g3_dummy_username>
2025-10-15 10:03:07 +0000

    Edit "Create TS from TO G3" test suite

--- Test Suites/Create TS from TO G3.ts
+++ Test Suites/Create TS from TO G3.ts
@@ -1,54 +1,73 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
-    <name>Create TS from TO G3</name>
-    <description></description>
-    <tag></tag>
-    <isRerun>false</isRerun>
-    <mailRecipient></mailRecipient>
-    <numberOfRerun>3</numberOfRerun>
-    <pageLoadTimeout>30</pageLoadTimeout>
-    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
-    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-    <rerunImmediately>true</rerunImmediately>
-    <testSuiteGuid>aa6947ba-e2ca-4671-95d6-d22da098ac92</testSuiteGuid>
-    <testCaseLink>
-        <guid>197357cd-4933-4065-bd47-14b11e921638</guid>
-        <isReuseDriver>false</isReuseDriver>
-        <isRun>true</isRun>
-        <testCaseId>Test Cases/Main Test Cases/Verify that data entered in a form is correctly saved and displayed after submission</testCaseId>
-        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-        <variableLink>
-            <testDataLinkId></testDataLinkId>
-            <type>DEFAULT</type>
-            <value></value>
-            <variableId>269060e5-49c4-4771-af87-4bebef169502</variableId>
-        </variableLink>
-        <variableLink>
-            <testDataLinkId></testDataLinkId>
-            <type>DEFAULT</type>
-            <value></value>
-            <variableId>bf303a06-fe36-4a87-b581-5dbfb9794c78</variableId>
-        </variableLink>
-    </testCaseLink>
-    <testCaseLink>
-        <guid>fc196556-9f46-4593-9242-65514f62b7c1</guid>
-        <isReuseDriver>false</isReuseDriver>
-        <isRun>true</isRun>
-        <testCaseId>Test Cases/Main Test Cases/Verify the user can reset their password via the Forgot Password link</testCaseId>
-        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-    </testCaseLink>
-    <testCaseLink>
-        <guid>36a824a6-5e48-4ac9-b2a9-7d77cd63acbe</guid>
-        <isReuseDriver>false</isReuseDriver>
-        <isRun>true</isRun>
-        <testCaseId>Test Cases/Main Test Cases/Verify the system displays an error message when incorrect password is entered</testCaseId>
-        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-    </testCaseLink>
-    <testCaseLink>
-        <guid>52b24a3b-cae9-4e0d-a3bb-cc0c2607ebef</guid>
-        <isReuseDriver>false</isReuseDriver>
-        <isRun>true</isRun>
-        <testCaseId>Test Cases/Main Test Cases/Verify the homepage loads within 2 seconds on a stable internet connection</testCaseId>
-        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-    </testCaseLink>
+   <name>Create TS from TO G3</name>
+   <description></description>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>aa6947ba-e2ca-4671-95d6-d22da098ac92</testSuiteGuid>
+   <testCaseLink>
+      <guid>197357cd-4933-4065-bd47-14b11e921638</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Cases/Verify that data entered in a form is correctly saved and displayed after submission</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>269060e5-49c4-4771-af87-4bebef169502</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>bf303a06-fe36-4a87-b581-5dbfb9794c78</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fc196556-9f46-4593-9242-65514f62b7c1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Cases/Verify the user can reset their password via the Forgot Password link</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>36a824a6-5e48-4ac9-b2a9-7d77cd63acbe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Cases/Verify the system displays an error message when incorrect password is entered</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>52b24a3b-cae9-4e0d-a3bb-cc0c2607ebef</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Cases/Verify the homepage loads within 2 seconds on a stable internet connection</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fdf7afcc-ef7a-4413-afb8-f73162397f5c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Common Test Cases/Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLinks>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>19fe967b-1564-4705-aff2-848d45b84489</variableId>
+      </variableLinks>
+      <variableLinks>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>75fa3d79-7e92-4d55-b359-f7c9493ea288</variableId>
+      </variableLinks>
+   </testCaseLink>
 </TestSuiteEntity>
