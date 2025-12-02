230d8498bfd4bae6c6e82abb876a30e9feecaa9f
lieu.huynh@katalon.com <katalon_g3_dummy_username>
2025-12-02 07:02:17 +0000

    Rename test case

--- Test Suites/Search Functionality Verification.ts
+++ Test Suites/Search Functionality Verification.ts
@@ -41,7 +41,7 @@
       <guid>09fe5f63-935b-4e5c-bf2a-9e951e9ce279</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Main Test Cases/Verify the system displays an error message when incorrect password is entered</testCaseId>
+      <testCaseId>Test Cases/Main Test Cases/Verify the system displays an error message when incorrect password is entered - renaming</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
