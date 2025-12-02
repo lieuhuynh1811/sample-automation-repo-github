66106e56b3786f8c0935bc8d4bfec6f0729156e1
lieu.huynh@katalon.com <katalon_g3_dummy_username>
2025-12-02 07:07:13 +0000

    Delete "API Integration Tests" test suite

--- Test Suites/TSC2-Error and Form.ts
+++ Test Suites/TSC2-Error and Form.ts
@@ -15,16 +15,6 @@
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
-         <testSuiteEntity>Test Suites/API Integration Tests</testSuiteEntity>
-      </TestSuiteRunConfiguration>
-      <TestSuiteRunConfiguration>
-         <configuration>
-            <groupName>Web Desktop</groupName>
-            <profileName>default</profileName>
-            <requireConfigurationData>false</requireConfigurationData>
-            <runConfigurationId>Chrome</runConfigurationId>
-         </configuration>
-         <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/User Authentication Suite</testSuiteEntity>
       </TestSuiteRunConfiguration>
    </testSuiteRunConfigurations>
