d40ec6b9d9b6dfccf347ce20e6cdf589a415cdf8
lieu.huynh@katalon.com <katalon_g3_dummy_username>
2025-12-02 07:00:39 +0000

    Delete "Verify the application logs out the user after 15 minutes of inactivity rename (1)" test case

--- Test Suites/Check CRUD TS.ts
+++ Test Suites/Check CRUD TS.ts
@@ -1,47 +1,40 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
-    <name>Check CRUD TS</name>
-    <description></description>
-    <tag></tag>
-    <isRerun>false</isRerun>
-    <mailRecipient></mailRecipient>
-    <numberOfRerun>3</numberOfRerun>
-    <pageLoadTimeout>30</pageLoadTimeout>
-    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
-    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-    <rerunImmediately>true</rerunImmediately>
-    <testSuiteGuid>3861ac3e-558c-4917-b0e6-9b7bc7225632</testSuiteGuid>
-    <testCaseLink>
-        <guid>50b89bd5-c11b-47b1-9b0a-b63b26477482</guid>
-        <isReuseDriver>false</isReuseDriver>
-        <isRun>true</isRun>
-        <testCaseId>Test Cases/Common Test Cases/Verify the application logs out the user after 15 minutes of inactivity rename (1)</testCaseId>
-        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-    </testCaseLink>
-    <testCaseLink>
-        <guid>763178a0-490f-44ea-bd22-b9cc59c8e8ac</guid>
-        <isReuseDriver>false</isReuseDriver>
-        <isRun>true</isRun>
-        <testCaseId>Test Cases/Common Test Cases/Login rename</testCaseId>
-        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-        <variableLink>
-            <testDataLinkId></testDataLinkId>
-            <type>DEFAULT</type>
-            <value></value>
-            <variableId>19fe967b-1564-4705-aff2-848d45b84489</variableId>
-        </variableLink>
-        <variableLink>
-            <testDataLinkId></testDataLinkId>
-            <type>DEFAULT</type>
-            <value></value>
-            <variableId>75fa3d79-7e92-4d55-b359-f7c9493ea288</variableId>
-        </variableLink>
-    </testCaseLink>
-    <testCaseLink>
-        <guid>90581773-b13b-45a2-a462-9487e1563d7f</guid>
-        <isReuseDriver>false</isReuseDriver>
-        <isRun>true</isRun>
-        <testCaseId>Test Cases/Common Test Cases/Verify the application logs out the user after 15 minutes of inactivity rename</testCaseId>
-        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-    </testCaseLink>
+   <name>Check CRUD TS</name>
+   <description></description>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>3861ac3e-558c-4917-b0e6-9b7bc7225632</testSuiteGuid>
+   <testCaseLink>
+      <guid>763178a0-490f-44ea-bd22-b9cc59c8e8ac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Common Test Cases/Login rename</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>19fe967b-1564-4705-aff2-848d45b84489</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>75fa3d79-7e92-4d55-b359-f7c9493ea288</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>90581773-b13b-45a2-a462-9487e1563d7f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Common Test Cases/Verify the application logs out the user after 15 minutes of inactivity rename</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
